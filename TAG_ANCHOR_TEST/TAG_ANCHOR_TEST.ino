562fa51c7e3ab3beb7bb41827ae92b9b0dc25b9e
MahmudMoon <mooncseru14@gmail.com>
2024-10-04 10:46:58 +0600

    mac id changed to ANCHOR[x]

--- TAG_ANCHOR_TEST/TAG_ANCHOR_TEST.ino
+++ TAG_ANCHOR_TEST/TAG_ANCHOR_TEST.ino
@@ -15,7 +15,7 @@ HardwareSerial MySerial(2);
 
 //const char* ssid = "DESKTOP-Q111QC8 5213";
 //const char* password = "t344T5$7";
- const int LED_PIN = 2;
+// const int LED_PIN = 2;
 //  const char* ssid = "BJIT_ADMIN";
 //  const char* password = "Bjit#@dmin";
 
@@ -29,11 +29,12 @@ const char* mqtt_server ="103.197.206.61";
 // const char* mqtt_password = "mqtt_password";
 const char* mqtt_clientid = "CLIENT1213";
 int count  = 0;
-int wifi = 5;
+int wifi = 2;
 int lastTime = 0;
 int lastTag2 = 0;
 int lastTag3 = 0;
 int lastTag4 = 0;
+int wifiRestart = 0;
 
 String macAddr = "";
 std::vector<String> liveDevices;  // Declare a dynamic array (vector)
@@ -87,19 +88,22 @@ void reconnect() {
         Serial.print("Attempting MQTT connection...");
 
         while (WiFi.status() != WL_CONNECTED) {
-          //digitalWrite(wifi, HIGH);
+          digitalWrite(wifi, HIGH);
           delay(250);
           Serial.print(".");
-          // digitalWrite(wifi, LOW);
+          digitalWrite(wifi, LOW);
           delay(250);
-          digitalWrite(LED_PIN, LOW);
-          setup_wifi();
+          wifiRestart+=1;
+          if(wifiRestart>=30){
+            wifiRestart = 0;
+            break;
+          }
         }
     
         // Attempt to connect
         if (client.connect(String(WiFi.macAddress()).c_str())) {
             Serial.println("connected");
-            digitalWrite(LED_PIN, HIGH);
+            digitalWrite(wifi, HIGH);
             client.subscribe("topic/liveDevices", 1);  // Subscribe with QoS 1 (or 0)
             client.publish("topic/liveDeviceStatus", "ask");
 
@@ -108,9 +112,9 @@ void reconnect() {
             Serial.print(client.state());
             Serial.println(" try again in 5 seconds");
             // Wait 5 seconds before retrying
-            digitalWrite(LED_PIN, LOW);
+            digitalWrite(wifi, LOW);
             delay(2500);
-            
+            setup_wifi();
         }
     }
 }
@@ -126,12 +130,15 @@ void setup_wifi() {
 
     
     while (WiFi.status() != WL_CONNECTED) {
-        //digitalWrite(wifi, HIGH);
+        digitalWrite(wifi, HIGH);
         delay(250);
         Serial.print(".");
-       // digitalWrite(wifi, LOW);
+        digitalWrite(wifi, LOW);
         delay(250);
-        digitalWrite(LED_PIN, LOW);
+        wifiRestart+=1;
+        if(wifiRestart>=40){
+          ESP.restart();
+        }
     }
     
     Serial.println("");
@@ -142,8 +149,7 @@ void setup_wifi() {
     Serial.print("MAC address: ");
     Serial.println(macAddr);
     // Light up the LED on pin 2 when connected
-    digitalWrite(LED_PIN, HIGH); // Turn on the LED
-   // digitalWrite(wifi, HIGH);
+    digitalWrite(wifi, HIGH);
 }
 
 
@@ -154,8 +160,8 @@ void setup() {
   MySerial.begin(115200, SERIAL_8N1, 16, 17);
 
   delay(1000);
-  //pinMode(wifi, OUTPUT);
-  pinMode(LED_PIN, OUTPUT); // Set pin 2 as output
+  pinMode(wifi, OUTPUT);
+  // pinMode(LED_PIN, OUTPUT); // Set pin 2 as output
   //digitalWrite(wifi, LOW);
 
   Serial.println("Sending AT command to IPS module...");
@@ -202,7 +208,7 @@ void loop() {
         }
 
         // If valid data is received, publish it
-        if (data != "") {
+        if (data != "" && !data.endsWith("#")) {
             Serial.println("Publishing data to MQTT...");
             client.publish("topic/test", data.c_str());
             count = 0;
@@ -270,7 +276,18 @@ String extractData(String input) {
     });
 
     // Prepare the result string
-    myRes.concat(macAddr);
+    //myRes.concat(macAddr);
+
+    if(macAddr == "D4:8A:FC:A4:E7:2C"){
+      myRes.concat("Anchor1");
+    }else if(macAddr == "D4:8A:FC:A5:01:E4"){
+      myRes.concat("Anchor2");
+    }else if(macAddr == "D4:8A:FC:A7:77:3C"){
+      myRes.concat("Anchor3");
+    }else if(macAddr == "D4:8A:FC:A8:9C:3C"){
+      myRes.concat("Anchor4");
+    }
+
     myRes.concat("#");
 
     for (const auto& pair : vec) {
@@ -285,160 +302,4 @@ String extractData(String input) {
 
     return myRes;
 }
-
-
-
-// void loop() {
-//    if (!client.connected()) {
-//         reconnect();
-//     }
-//   client.loop();
-//   String data = "";
-  
-  
-//   for (int i = 0; i < liveDevices.size(); i++) {
-
-//     // Serial.print("Element ");
-//     // Serial.print(i);
-//     // Serial.print(": ");
-//     // Serial.println(liveDevices[i]);
-
-//     Serial.println("AT+ANCHOR_SEND="+liveDevices[i]+",4,Test");
-
-//     MySerial.println("AT+ANCHOR_SEND="+liveDevices[i]+",4,Test");
-//     delay(100);
-  
-   
-//   }
-  
-  
-//   //  MySerial.println("AT+ANCHOR_SEND=Tag1,4,Test");
-//     // delay(200);
-
-//   //  MySerial.println("AT+ANCHOR_SEND=Tag2,4,Test");
-//   //   delay(100);
-
-  
-
-//   Serial.println("OUTSIDE LOOP");
-
-//   while(MySerial.available()){
-//       String response = MySerial.readString();
-      
-//       data.concat(extractData(response));
-//       //count+=1;
-
-//       if (response.indexOf("OK") != -1) {
-//         Serial.println("Communication with IPS Module Successful");
-//       }
-//       {
-//      Serial.println(data);
-//      Serial.println(count);
-//       if(data!=""){
-//         client.publish("topic/test", data.c_str());
-//         count=0;
-//         unsigned long currentTime = millis();  // Get current time in milliseconds
-//         unsigned long diff = currentTime - lastTime;
-//         Serial.print("Difference: ");
-//         Serial.println(diff);
-//         lastTime = currentTime;
-//       }
-//         break;
-//       }
-//     }
-
-//  delay(1000);
-
-// }
-
-
-// String extractData(String input){
-
-// String myRes = "";
-
-// const int maxValues = 10; // Maximum number of values to store
-// int values[maxValues];
-// String tags[maxValues];
-// int valuesCount = 0;
-
-//   String delimiter = "cm";
-//   int startIndex = 0;
-//   int endIndex = 0;
-
-
-//   while ((endIndex = input.indexOf(delimiter, startIndex)) != -1) {
-//     // Find the part before "cm"
-//     String part = input.substring(startIndex, endIndex);
-    
-//     // Find the tag
-//     int tagStartIndex = part.indexOf("Tag");
-//     int tagEndIndex = part.indexOf(',', tagStartIndex);
-//     String tag = part.substring(tagStartIndex, tagEndIndex);
-    
-//     // Find the last comma in this part to locate the number
-//     int numberStartIndex = part.lastIndexOf(',') + 1;
-//     // Extract the number
-//     String number = part.substring(numberStartIndex);
-    
-//     // Store the tag and number in the arrays
-//     if (valuesCount < maxValues) {
-//       tags[valuesCount] = tag;
-//       values[valuesCount] = number.toInt();
-//       valuesCount++;
-//     }
-    
-//     // Update startIndex to continue searching
-//     startIndex = endIndex + delimiter.length();
-//   }
-
-//   // Print the extracted values and tags
-//  // Serial.println("Extracted values and tags:");
-
-//   std::map<String, int> myMap;
-
-//   Serial.print("Tags and Values");
-//   for (int i = 0; i < valuesCount; i++) {  
-//     myMap.insert(std::make_pair(tags[i], values[i]));
-
-//   }
-
-
-//   // for (auto it = myMap.begin(); it != myMap.end(); ++it) {
-//   //   Serial.print("Key: ");
-//   //   Serial.print(it->first);
-//   //   Serial.print(", Value: ");
-//   //   Serial.println(it->second);
-//   // }
-
-// // Create a vector of pairs from the map
-//   std::vector<std::pair<String, int>> vec;
-//   for (const auto& pair : myMap) {
-//       vec.push_back(pair);
-//   }
-
-//      // Sort the vector based on the values (second part of pair)
-//   std::sort(vec.begin(), vec.end(),
-//       [](const std::pair<String, int>& a, const std::pair<String, int>& b) {
-//           return a.second < b.second;
-//       });
-
-
-//  // Serial.println("\nSorted map by values:");
-//   myRes.concat(macAddr);
-//   myRes.concat("#");
-
-//   if(vec.size() >= 2 ){
-//     for (const auto& pair : vec) {
-//       Serial.print(pair.first.c_str());
-//       Serial.print(": ");
-//      // Serial.println(pair.second);
-//       myRes.concat(pair.first.c_str());
-//       myRes.concat(":");
-//       myRes.concat(pair.second);
-//       myRes.concat("\n");
-//     }
-//   }
-
-//   return myRes;
-// }
 
